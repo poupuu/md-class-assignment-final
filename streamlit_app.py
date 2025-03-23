f99c3e74b86c356a823891f34ecdaf806e2d1a6e
poupuu <williamanwar2@gmail.com>
2025-03-23 23:35:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ from sklearn.preprocessing import RobustScaler
 label_encoder = joblib.load("label_encoder.pkl")
 standard_scaler = joblib.load("standard_scaler.pkl")
 scaling = joblib.load("robust_scaler-2.pkl")
+robust_scaler = joblib.load("robust_scaler-2.pkl")
 model = joblib.load("fine_tuned_model.pkl")
 
 def input_user_to_df(input):
