4565f98e1c6b5e0530b035dcb4daad25db4d2144
poupuu <williamanwar2@gmail.com>
2025-03-23 18:15:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,6 +86,7 @@ def main():
     user_input = [Gender, Age, Height, Weight, family_history_with_overweight, FAVC, FCVC, NCP, CAEC, SMOKE, CH2O, SCC, FAF, TUE, CALC, MTRANS]
 
     st.write('Data input by user')
+    user_input
     user_input_encoded = encode(user_input, label_encoder)
     st.table(user_input_encoded)
     
